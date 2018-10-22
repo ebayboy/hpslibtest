e93ad81b7e61a07028e56b47c0b2b12cdc92cb90
fanpengfei <fanpengfei@jd.com>
2018-10-22 02:55:43 -0400

    fixbug: 修复在栈上申请内存过大引起coredump问题

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "hpslib.h"
 #include "cfg_parser.h"
@@ -11,13 +13,21 @@ int main(int argc, char **args)
     libtest(filename);
 
     int rc = 0;
-        
-    rc = cfg_parser_parse2(filename);
-    printf("rc=%d\n", rc);
-    
-    rc = cfg_parser_parse(filename);
+
+    waf_t *waf = NULL;
+
+    if ((waf = malloc(sizeof(waf_t))) == NULL) {
+        return -1;
+    }
+    memset(waf, 0, sizeof(waf));
+
+    rc = cfg_parser_parse(filename, waf);
 
     printf("rc=%d\n", rc);
+
+    if (waf) {
+        free(waf);
+    }
 
     return 0;
 }
