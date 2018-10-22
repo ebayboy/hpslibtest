2985c883dccf5317f705db7ba7dec883b371e505
fanpengfei <fanpengfei@jd.com>
2018-10-21 22:26:19 -0400

    fw

--- main.c
+++ main.c
@@ -8,9 +8,14 @@ int main(int argc, char **args)
     printf("hello world!\n");
     char *filename="hpslib.json";
 
-    libtest();
+    libtest(filename);
 
-    int rc = cfg_parser_parse(filename);
+    int rc = 0;
+        
+    rc = cfg_parser_parse2(filename);
+    printf("rc=%d\n", rc);
+    
+    rc = cfg_parser_parse(filename);
 
     printf("rc=%d\n", rc);
 
