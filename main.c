ad60aa4e770e6780e2c574b104c790e995cdca13
fanpengfei <fanpengfei@jd.com>
2018-10-29 03:39:13 -0400

    up

--- main.c
+++ main.c
@@ -2,18 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "hpslib.h"
-#include "waf_config.h"
+//#include "waf_config.h"
 #include "waf.h"
-#include "common.h"
+//#include "common.h"
 
 int main(int argc, char **args)
 {
-    printf("hello world!\n");
     char *filename="hpslib.json";
-
-    libtest(filename);
-
     int rc = 0;
 
     /* 1. init */
@@ -34,6 +29,8 @@ int main(int argc, char **args)
             &matched_rule_id);
 
     printf("rc=%d matched_rule_id:%d\n", rc, matched_rule_id);
+
+
 
     /* 4. waf_fini */
     waf_fini();
