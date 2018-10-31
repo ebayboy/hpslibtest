0b3c73a9b58da7cdc0247bb77183f46c6e8782fd
fanpengfei <fanpengfei@jd.com>
2018-10-31 03:09:42 -0400

    fw

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "waf.h"
 //#include "common.h"
 
-int main(int argc, char **args)
+int main()
 {
     char *filename="hpslib.json";
     int rc = 0;
@@ -19,18 +19,26 @@ int main(int argc, char **args)
     /* 2. show */
     waf_show();
 
+
+    /* waf data */
+    char *uri = "http://192.168.137.200/?name=/etc/passwd";
+    char *args = "name=/etc/passwd&a=b";
+    char *request_body = NULL;
+
+    /* add header var */
+
+    void *data =  waf_data_create(HTTP_GET, uri, strlen(uri), args, strlen(args), request_body, 0);
+
     /* 3. waf_match */
 
-    char *buff = "http://192.168.137.200/?name=/etc/passwd";
     int matched_rule_id = 0;
-    rc = waf_match("$u_uri",
-            buff, 
-            strlen(buff), 
-            &matched_rule_id);
 
+    rc = waf_match(data, &matched_rule_id);
     printf("rc=%d matched_rule_id:%d\n", rc, matched_rule_id);
 
-
+    if (data) {
+        waf_data_destroy(data);
+    }
 
     /* 4. waf_fini */
     waf_fini();
