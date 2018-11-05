4b397917e8ea510738a685442c1c7dbb25726969
fanpengfei <fanpengfei@jd.com>
2018-11-04 20:29:37 -0500

    todo

--- main.c
+++ main.c
@@ -13,12 +13,12 @@ int main()
 
     /* 1. init */
     if (waf_init("/var/log/waf.log", "hpslib.json") == -1) {
+        fprintf(stderr, "Error: init\n");
         return -1;
     }
 
     /* 2. show */
     waf_show();
-
 
     /* waf data */
     char *uri = "http://192.168.137.200/?name=/etc/passwd";
@@ -42,6 +42,16 @@ int main()
             WAF_HDR_REFERER, strlen(WAF_HDR_REFERER),
             referer , strlen(referer)) == -1) {
         fprintf(stderr, "Error: waf_data_add_param referer error.");
+        goto out;
+    }
+
+    /* add var */
+    char *referer_var = "$http_referer";
+    if (waf_data_add_param(data,
+            PARAM_VAR_TYPE,
+            referer_var, strlen(referer_var), 
+            WAF_HDR_REFERER, strlen(WAF_HDR_REFERER)) == -1) {
+        fprintf(stderr, "Error: waf_data_add_param referer var error.");
         goto out;
     }
 
