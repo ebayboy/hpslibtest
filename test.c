099943a0050b57982ff8aa01089addf0ef21d44d
fanpengfei <fanpengfei@jd.com>
2018-11-07 03:12:47 -0500

    update for headers & vars & mzs

--- test.c
+++ test.c
@@ -362,36 +362,28 @@ int main()
     char *testconf="test.json";
     char *log_file="/var/log/waf_test.log";
     int rc = 0;
-
-    /*set log */
     FILE *log_fp = NULL;
 
-
+    /* init log */
     if ((log_fp = fopen(log_file, "a+")) == NULL) {
-        fprintf(stderr, "open log file [%s] error.\n", log_file);
+        log_info("open log file [%s] error.\n", log_file);
         return -1;
     }
-
     log_set_fp(log_fp);
 
-    /* 1. init */
+    /* waf init */
     if (waf_init("/var/log/waf.log", "hpslib.json") == -1) {
-        fprintf(stderr, "Error: init\n");
+        log_info("Error: init");
         return -1;
     }
 
-    /* 2. show */
+    /* waf show */
     waf_show();
 
     /* load test.json */
-    char *args = "";
-    char *uri = "";
-    char *request_body = "";
-    char *cookies = "";
-
-    void *data;
-
-    test_config_t *cfg;
+    void *data = NULL;
+    test_config_t *cfg = NULL;
+    int matched_rule_id = 0;
 
     if ((cfg = test_config_create()) == NULL) {
         rc = -1;
@@ -405,75 +397,12 @@ int main()
 
     test_config_show(cfg);
 
-    if (data == NULL) {
-        log_error("Error: data is NULL!");
-        goto out;
-    }
-
-
-    /* TODO */
-#if 0
-    char *tmp = "tmp12\%203abc";
-    if (waf_data_add_param(data,
-                PARAM_HDR_TYPE,
-            "tmp", strlen(tmp),
-            tmp , strlen(tmp)) == -1) {
-        fprintf(stderr, "Error: waf_data_add_param ua error.");
-        goto out;
-    }
-
-    /* add header */
-    char *ua = "1122\%20334455";
-    if (waf_data_add_param(data,
-            PARAM_HDR_TYPE,
-            WAF_HDR_UA, strlen(WAF_HDR_UA),
-            ua , strlen(ua)) == -1) {
-        fprintf(stderr, "Error: waf_data_add_param ua error.");
-        goto out;
-    }
-
-    //char *referer = "referer-/etc/passwd445566";
-    char *referer = "referer";
-    if (waf_data_add_param(data,
-            PARAM_HDR_TYPE,
-            WAF_HDR_REFERER, strlen(WAF_HDR_REFERER),
-            referer , strlen(referer)) == -1) {
-        fprintf(stderr, "Error: waf_data_add_param referer error.");
-        goto out;
-    }
-
-    /* add var */
-    char *referer_var = "$http_referer";
-    if (waf_data_add_param(data,
-            PARAM_MZ_TYPE,
-            referer_var, strlen(referer_var), 
-            WAF_HDR_REFERER, strlen(WAF_HDR_REFERER)) == -1) {
-        fprintf(stderr, "Error: waf_data_add_param referer var error.");
-        goto out;
-    }
-
-    char *ua_var = "$u_user_agent";
-    if (waf_data_add_param(data,
-                PARAM_MZ_TYPE,
-                ua_var, strlen(ua_var), 
-                WAF_HDR_UA, strlen(WAF_HDR_UA)) == -1) {
-        fprintf(stderr, "Error: waf_data_add_param referer var error.");
-        goto out;
-    }
-#endif
-
     /* waf data show */
     waf_data_show(data);
 
-    /* 3. waf_match */
-    int matched_rule_id = 0;
-
+    /* waf match */
     rc = waf_match(data, &matched_rule_id);
-    printf("rc=%d matched_rule_id:%d\n", rc, matched_rule_id);
-
-    if (data) {
-        waf_data_destroy(data);
-    }
+    log_info("rc=%d matched_rule_id:%d\n", rc, matched_rule_id);
 
 out:
 
@@ -483,7 +412,13 @@ out:
         fclose(log_fp);
     }
 
-    test_config_free(cfg);
+    if (cfg) {
+        test_config_free(cfg);
+    }
+
+    if (data) {
+        waf_data_destroy(data);
+    }
 
     return 0;
 }
